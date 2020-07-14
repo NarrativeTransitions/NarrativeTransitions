2aa2fe9c635ce66b23fe1f74ab1c2f0492576db1
NarrativeTransitions <tangjxhugh@126.com>
2020-07-14 15:16:40 +0800

    commi

--- src/component/Usecase.js
+++ src/component/Usecase.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Player,ControlBar} from 'video-react'
+import {Player,ControlBar,PlaybackRateMenuButton} from 'video-react'
 import "../css/video-react.css";
 import style from '../css/Usecase.module.scss'
 
@@ -19,6 +19,7 @@ class Usecase extends React.Component{
                         <span>
                             Here is a use case on leveraging the taxonomy to design a data video. The video is about COVID-19, which introduces the severe condition of the COVID-19 spreading by presenting a series of data facts with elaborate transitions.
                         </span>
+
                         {/*<span className={style.quote}> &nbsp;"</span>*/}
                     </div>
                 </div>
@@ -27,8 +28,11 @@ class Usecase extends React.Component{
                     playsInline
                     // src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
                     src={require("../assets/video/usecase.mp4")}>
-                    <ControlBar autoHide={false} />
+                    <ControlBar autoHide={false}>
+                        <PlaybackRateMenuButton rates={[2, 1.5, 1, 0.5, 0.1]} order={7.1} />
+                    </ControlBar>
                 </Player>
+
             </div>
 
         );
