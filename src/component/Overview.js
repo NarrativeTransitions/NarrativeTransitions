0c8f178c6cc2db5ecea2d59b7365a7d93b6c7ec0
NarrativeTransitions <tangjxhugh@126.com>
2020-07-14 13:51:18 +0800

    commit

--- src/component/Overview.js
+++ src/component/Overview.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import style from '../css/Overview.module.scss'
 import PDF from 'react-pdf-js'
-const pdfurl = require('../assets/taxonomy_grey.pdf');
+// const pdfurl = require('../assets/taxonomy_grey.pdf');
 
 class Overview extends React.Component {
 
     render() {
         return (
             <div className={style.container}>
-                <PDF file={pdfurl} scale="1.2" />
+                <img src="assets/image/overview.png"/>
+                {/*<PDF file={pdfurl} scale="1.2" />*/}
                 <div className={style.desc}>
                     <strong>a-e:</strong> Taxonomy of transition designs in data videos.<br/>
                     <strong>f:</strong> Changed and unchanged visual variables of Preserving Guide and Narrative Agent
--- src/component/Usecase.js
+++ src/component/Usecase.js
@@ -8,6 +8,21 @@ class Usecase extends React.Component{
     render() {
         return (
             <div className={style.video}>
+
+                {/*define container*/}
+                <div className={style.defContainer}>
+                    <div className={style.def}>
+                        {/*<span className={style.quote}>" </span>*/}
+                        {/*<span className={style.title}>&nbsp;&nbsp;&nbsp;*/}
+                            {/*{store.getState()["sampleList"][i]["category"]}*/}
+                            {/*&nbsp;&nbsp;</span>*/}
+                        <span>
+                            Here is a use case on leveraging the taxonomy to design a data video. The video is about COVID-19, which introduces the severe condition of the COVID-19 spreading by presenting a series of data facts with elaborate transitions.
+                        </span>
+                        {/*<span className={style.quote}> &nbsp;"</span>*/}
+                    </div>
+                </div>
+
                 <Player
                     playsInline
                     // src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
